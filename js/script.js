7a13fda014f8a4fb3b933bf115674109c59a9c33
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-12-13 18:07:42 +0200

    Make navigation sticky

--- js/script.js
+++ js/script.js
@@ -27,12 +27,40 @@ allLinks.forEach((link) => {
       });
     }
 
-    if (href !== "#" && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href)
-      sectionEl.scrollIntoView({behavior: 'smooth'})
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
     }
   });
 });
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky')
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky')
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px'
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
